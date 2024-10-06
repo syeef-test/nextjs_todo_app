78c0b62f8a55617d68e7512930c8da91ac848eef
syeef <syeefislam@yahoo.com>
2024-10-06 19:05:07 +0530

    add task done in localstorage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function index() {
-  return <div>index</div>;
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
+
+  const addTask = () => {
+    setTasks([...tasks, { text: taskInput, completed: false }]);
+    setTaskInput("");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <>
+      <div>
+        <h1>To Do App</h1>
+        <div>
+          <input
+            type="text"
+            value={taskInput}
+            onChange={(e) => setTaskInput(e.target.value)}
+            placeholder="Enter a new task"
+          />
+          <button onClick={addTask}>Add Task</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default index;
